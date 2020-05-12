43b7c344ad336accea3174f7fcc5fd03ce6b7b2b
Ryan Hafen <rhafen@gmail.com>
2020-05-12 14:06:29 -0700

    Initial commit

---
+++ R/app.R
@@ -0,0 +1,153 @@
+
+#' @export
+register_app <- function(name, path = tempfile()) {
+  if (!dir.exists(path))
+    dir.create(path)
+
+  structure(list(
+    name = name,
+    path = path
+  ), class = c("registered_app", "list"))
+}
+
+# reads data and sends it to trelliscope in a temporary directory
+build_casecount_display <- function(
+  app,
+  sources,
+  name,
+  desc,
+  ref_source = NULL,
+  nrow = 2, ncol = 3,
+  thumb = FALSE,
+  state = NULL,
+  views = NULL,
+  id = NULL,
+  order = 1
+  # source_ids = NULL,
+  # md_desc = md,
+  # tr_detail = "md_object",
+  # geo_name,
+  # geo_id
+) {
+  if (!inherits(app, "registered_app"))
+    stop("'app' not a valid object. See register_app().", call. = FALSE)
+
+  if (is.null(ref_source))
+    ref_source <- sources[[1]]$source
+
+  # assemble data
+  d <- get_casecount_data(sources, ref_source)
+  # d$data[[1]] %>% group_by(source) %>% summarise(max_date = max(date))
+
+  # add cognostics
+  dc <- d %>%
+    mutate(cogs = map2_cog(data, population, get_cogs))
+
+  # get limits
+  lims <- get_lims(d)
+
+  # add geocard panels
+  tmp <- geocard(dc[1, ], ref_source = ref_source,
+    geo_level = get_geo_level(sources))
+
+  dc$panel <- lapply(seq_len(nrow(dc)), function(i) {
+    geocard(dc[i, ], ref_source = ref_source,
+      geo_level = get_geo_level(sources))
+  })
+  class(dd$panel) <- c("trelliscope_panels", "list")
+  cnms <- setdiff(names(dd), "flag_url")
+  dd <- dd %>% ungroup() %>% select(one_of(cnms))
+
+
+  # make plot
+
+  # y_domain = NULL, y_log_domain = NULL, 
+
+
+  p <- trelliscope(dd,
+    name = tr_name,
+    desc = tr_desc,
+    md_desc = md_desc,
+    path = dr,
+    state = state,
+    # md_desc = tr_
+    width = 500, height = 416,
+    thumb = thumb,
+    views = views,
+    id = id,
+    order = order,
+    nrow = nrow, ncol = ncol)
+
+  print(p)
+}
+
+# 
+deploy_app <- function() {
+  if (!inherits(x, "casecount_built_app"))
+    stop("Not a valid object.", call. = FALSE)
+
+}
+
+get_geo_level <- function(sources) {
+  geo_level <- sources[[1]]$admin_level
+  if (geo_level %in% c(0:2)) 
+    geo_level <- paste0("admin", geo_level)
+  geo_level
+}
+
+# internal
+get_casecount_data <- function(sources, ref_source) {
+  lvl <- unique(unlist(lapply(sources, function(x) x$admin_level)))
+  if (length(lvl) > 1)
+    stop("Cannot assemble data that comes from multiple admin levels.",
+      call. = FALSE)
+
+  ids <- unlist(lapply(sources, function(x) x$source_id))
+  if (length(unique(ids)) != length(sources))
+    stop("Each source must have a unique source_id.\nFound: ",
+      paste(ids, collapse = ","), " but there are ",
+      length(sources), " sources.",
+      call. = FALSE)
+  d <- lapply(sources, function(x) {
+    suppressMessages(readr::read_csv(x$file)) %>%
+      dplyr::mutate(source = x$source_id)
+  }) %>%
+  dplyr::bind_rows()
+
+  if (!ref_source %in% ids)
+    stop("ref_source: ", ref_source, " is not in the list of sources.",
+      call. = FALSE)
+  ids <- c(ref_source, setdiff(ids, ref_source))
+  d$source <- factor(d$source, levels = ids)
+
+  # nest
+  code_vars <- names(d)[grepl("_code$", names(d))]
+  d <- d %>%
+    dplyr::group_by_at(code_vars) %>%
+    tidyr::nest() %>%
+    dplyr::ungroup()
+
+  # merge geo data
+  geo_obj <- get_geo_level(sources)
+
+  geo_dat <- try(get(geo_obj, as.environment("package:geoutils")))
+  if (inherits(geo_dat, "try-error"))
+    stop("Could not find data in geoutils package matching admin_level: ", lvl,
+      call. = FALSE)
+
+  d2 <- dplyr::inner_join(d, geo_dat, by = code_vars)
+
+  if (nrow(d2) < nrow(d)) {
+    a <- dplyr::anti_join(
+      dplyr::select(d, tidyselect::one_of(code_vars)),
+      dplyr::select(geo_dat, tidyselect::one_of(code_vars)),
+      by = code_vars
+    )
+    message("Data removed for the following geographic regions because ",
+      "matching geo data in '", geo_obj, "' was not found.")
+    message(paste(format(a)[-3], collapse = "\n"))
+  }
+
+  class(d2) <- c(class(d2), "casecounts")
+  d2
+}
